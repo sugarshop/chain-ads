06f1e65b4463eeda28afe9539b02a66e4702b222
RQ <mengmengmengqiang@gmail.com>
2024-07-16 19:37:04 +0800

    [init]: init a chain-ads func smart contract project

---
+++ scripts/deployChainAds.ts
@@ -0,0 +1,21 @@
+import { toNano } from '@ton/core';
+import { ChainAds } from '../wrappers/ChainAds';
+import { compile, NetworkProvider } from '@ton/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const chainAds = provider.open(
+        ChainAds.createFromConfig(
+            {
+                id: Math.floor(Math.random() * 10000),
+                counter: 0,
+            },
+            await compile('ChainAds')
+        )
+    );
+
+    await chainAds.sendDeploy(provider.sender(), toNano('0.05'));
+
+    await provider.waitForDeploy(chainAds.address);
+
+    console.log('ID', await chainAds.getID());
+}
---
+++ wrappers/ChainAds.ts
@@ -0,0 +1,66 @@
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+
+export type ChainAdsConfig = {
+    id: number;
+    counter: number;
+};
+
+export function chainAdsConfigToCell(config: ChainAdsConfig): Cell {
+    return beginCell().storeUint(config.id, 32).storeUint(config.counter, 32).endCell();
+}
+
+export const Opcodes = {
+    increase: 0x7e8764ef,
+};
+
+export class ChainAds implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new ChainAds(address);
+    }
+
+    static createFromConfig(config: ChainAdsConfig, code: Cell, workchain = 0) {
+        const data = chainAdsConfigToCell(config);
+        const init = { code, data };
+        return new ChainAds(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendIncrease(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            increaseBy: number;
+            value: bigint;
+            queryID?: number;
+        }
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.increase, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .storeUint(opts.increaseBy, 32)
+                .endCell(),
+        });
+    }
+
+    async getCounter(provider: ContractProvider) {
+        const result = await provider.get('get_counter', []);
+        return result.stack.readNumber();
+    }
+
+    async getID(provider: ContractProvider) {
+        const result = await provider.get('get_id', []);
+        return result.stack.readNumber();
+    }
+}
